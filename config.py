ff5b673af34ef2eb69b9860f329e5a4ae8751e7f
sudhircoderview <149396740+sudhircoderview@users.noreply.github.com>
2024-08-30 00:15:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# Astra DB API credentials and endpoints
+ASTRA_API_TOKEN = ""
+ASTRA_API_ENDPOINT = ""
+ASTRA_DB_TOKEN = ""
+
+# Amazon Bedrock credentials
+AWS_ACCESS_KEY = ""
+AWS_SECRET_KEY = ""
+AWS_REGION_NAME = ""
+AWS_MODEL_ID = ""
+
+# File paths
+VECTOR_DB_FLOW_FILE = "vector_db.json"
+CHAT_FLOW_FILE = "Vector_Store_RAG.json"
+TEMP_FILES_DIR = "temp_files"
